8dd1d42936128775f1aadc715e5a001b9a886bd1
Fernando N <64892153+nathanaelfd10@users.noreply.github.com>
2023-02-24 14:20:53 +0700

    Initial commit (Part II)

---
+++ src/main/java/com/noxfl/momijileafrake/MomijiLeafRakeApplication.java
@@ -0,0 +1,13 @@
+package com.noxfl.momijileafrake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomijiLeafRakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomijiLeafRakeApplication.class, args);
+	}
+
+}
