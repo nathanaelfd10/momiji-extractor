55ce4ffe6bdd1de7f981b0f2744495dff1ddd870
Fernando N <64892153+nathanaelfd10@users.noreply.github.com>
2023-03-14 13:56:31 +0700

    Fix typo ContextExtractoryFactoryImpl -> ContentExtractoryFactoryImpl

--- src/main/java/com/noxfl/woodchipper/extractor/impl/ContentExtractoryFactoryImpl.java
+++ src/main/java/com/noxfl/woodchipper/extractor/impl/ContentExtractoryFactoryImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ContextExtractoryFactoryImpl implements ContentExtractorFactory {
+public class ContentExtractoryFactoryImpl implements ContentExtractorFactory {
 
     @Autowired
     private JsonContentExtractor jsonContentExtractorImpl;
